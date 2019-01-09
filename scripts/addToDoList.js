488d16cebdcab5aadcd1e5e206da2228dfc1337b
VitaliiBerezhnov <vitaliiberezhnov@gmail.com>
2019-01-09 15:10:47 +0200

    add module

--- scripts/addToDoList.js
+++ scripts/addToDoList.js
@@ -1,63 +1,54 @@
 
-define(function(){
+define(function(require){
 
-        function removeSpan (event) {
-        
-            var target = event.target;
-    
-            if(target.tagName != "SPAN") return
-            var el = target.parentNode;   
-            el.parentNode.removeChild(el)   
-        };
+    var removeSpan = require('scripts/removeSpan')
+    var doneTask = require('scripts/doneTask')
 
-        function doneTask(event) {
-        
-            var target = event.target;
-    
-            if(target.tagName != "LI") return
-            target.classList.toggle("checked");
-            
-        };
+    return function addToDoList(node){
 
-        return function addToDoList(node){
-
-            var clearList = node.querySelector('#clearList');
-            clearList.onclick = function(){ 
+        var clearList = node.querySelector('#clearList');
+        clearList.onclick = function(){ 
                 var allToDo = document.getElementById('allToDo'); 
                 allToDo.removeChild(node) 
-            };
-            
-            var task = node.querySelector('#input_text');
-            var ul = node.querySelector('#toDoList');
-
-
-            var addBtn = node.querySelector('#input_button');
-            addBtn.onclick = function() {
-                if(!task.value) return;
-
-                var li = document.createElement('li');
-                var textTask = document.createTextNode(task.value)
-
-                var span = document.createElement('span')
-                var text = document.createTextNode('\u2715')
-
-                span.className = 'close';
-                span.appendChild(text);
-
-                li.appendChild(textTask);
-                li.appendChild(span);
-                ul.appendChild(li);  
-
-                task.value = '';
-                task.focus();   
-            }
-
-            ul.addEventListener ('click', removeSpan );
-            ul.addEventListener ('click', doneTask );
-
-            var clearBtm = node.querySelector('#clearBtm');
-            clearBtm.onclick = function() {
-                ul.innerHTML = '';
-            };
         };
+            
+        var task = node.querySelector('#input_text');
+        var ul = node.querySelector('#toDoList');
+
+
+        var addBtn = node.querySelector('#input_button');
+        addBtn.onclick = function() {
+            if(!task.value) return;
+
+            var li = document.createElement('li');
+            var textTask = document.createTextNode(task.value)
+
+            var span = document.createElement('span')
+            var text = document.createTextNode('\u2715')
+
+            span.className = 'close';
+            span.appendChild(text);
+
+            li.appendChild(textTask);
+            li.appendChild(span);
+            ul.appendChild(li);  
+
+            task.value = '';
+            task.focus();   
+        }
+
+        ul.addEventListener ('click', removeSpan );
+        ul.addEventListener ('click', doneTask );
+        ul.innerHTML = '';
+
+        var clearBtm = node.querySelector('#clearBtm');
+        clearBtm.onclick = function() {
+            ul.innerHTML = '';
+        };
+            
+            
+        var nameTask = node.querySelector('#nameTask');  
+        nameTask.focus();         
+            
+    };
 });
